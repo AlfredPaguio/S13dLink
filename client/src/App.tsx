5d9a545afe67013648451ae985819eab01273a68
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2024-04-02 13:25:49 +0800

    [Client] Refactor: Use proper way of creating forms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,66 +1,29 @@
-import { useState } from "react";
+import URLInputForm from "./components/URLInputForm";
 import { Button } from "./components/ui/button";
+import { Card } from "./components/ui/card";
 
 function App() {
-  const [originalUrl, setOriginalUrl] = useState("");
-  const [shortenedUrl, setShortenedUrl] = useState("");
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setOriginalUrl(event.target.value);
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/add", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ originalUrl: originalUrl }),
-      });
-      const { shortUrl } = await response.json();
-      setShortenedUrl(shortUrl);
-    } catch (error) {
-      console.error("Error creating shortened URL:", error);
-    }
-  };
-
   return (
-    <div className="w-full h-screen bg-neutral-100 dark:bg-gray-800 p-8">
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-2xl font-semibold text-gray-900 dark:text-white mb-6">
+    <div className="flex flex-col items-center justify-center w-full h-svh bg-neutral-500 p-8">
+      <Card className="flex flex-col items-center justify-center p-6">
+        <h1 className="text-2xl font-semibold text-black mb-6">
           URL Shortener
         </h1>
-        <form className="w-full max-w-md" onSubmit={handleSubmit}>
-          <div className="rounded-md shadow-sm">
-            <input
-              aria-label="URL input"
-              className="block w-full text-lg py-3 px-4 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white"
-              type="text"
-              placeholder="https://www.example.com"
-              value={originalUrl}
-              onChange={handleInputChange}
-            />
-          </div>
-          <Button className="w-full mt-4 py-2 rounded-b-md" type="submit">
-            Shorten URL
-          </Button>
-        </form>
-        <div className="w-full max-w-md mt-8">
-          <h2 className="text-lg font-medium text-gray-900 dark:text-white mb-4">
+        <URLInputForm />
+        <div className="w-full max-w-lg mt-8">
+          <h2 className="text-lg font-medium text-black mb-4">
             All Shortened URLs
           </h2>
           <div className="space-y-4">
-            {shortenedUrl && (
+            {/* {shortenedUrl && (
               <a
                 href={shortenedUrl}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-md"
+                className="flex items-center justify-between bg-gray-100 p-4 rounded-md"
               >
                 <div>
-                  <p className="text-sm text-gray-900 dark:text-white mb-1">
+                  <p className="text-sm text-gray-900 mb-1">
                     {shortenedUrl}
                   </p>
                   <p className="text-xs text-gray-500">Clicked 999999 times</p>
@@ -69,7 +32,7 @@ function App() {
                   Copy
                 </Button>
               </a>
-            )}
+            )} */}
             <div className="flex items-center justify-between bg-gray-100 dark:bg-gray-700 p-4 rounded-md">
               <div>
                 <p className="text-sm text-gray-900 dark:text-white mb-1">
@@ -88,7 +51,7 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
+      </Card>
     </div>
   );
 
---
+++ client/src/components/URLInputForm.tsx
@@ -0,0 +1,73 @@
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "./ui/input";
+
+const FormSchema = z.object({
+  websiteURL: z.string().url({ message: "Invalid url" }),
+});
+
+function URLInputForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      websiteURL: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof FormSchema>) {
+    console.log(values);
+    const { websiteURL } = values;
+    try {
+      const fetchResponse = await fetch("/api/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ originalUrl: websiteURL }),
+      });
+      const data = await fetchResponse.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error creating shortened URL:", error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form className="space-y-2" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="websiteURL"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Website URL</FormLabel>
+              <FormControl>
+                <Input placeholder="https://www.example.com" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the original URL that you want to shorten.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          Shorten URL
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default URLInputForm;
