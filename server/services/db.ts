edb43ccc4b45f841ef91c99e78b6b95017c46106
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2024-11-13 21:47:55 +0800

    [Server] Migrate "db.ts" to use PostgreSQL

--- server/services/db.ts
+++ server/services/db.ts
@@ -1,18 +1,31 @@
-import mongoose from "mongoose";
+import { Client } from "pg";
 
 export async function connectToDatabase() {
-  // await mongoose.connect("mongodb://127.0.0.1:27017/mongoose-app");
   try {
-    await mongoose.connect(Bun.env.DB_URL);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const client = new Client({
+      connectionString: Bun.env.DB_URL,
+    });
+    await client.connect();
+    console.log("Connected to PostgreSQL");
+    return client;
   } catch (error) {
-    console.error("[Server] Error: " + error);
-    process.exit(1);
+    console.error("[Server] Error connecting to PostgreSQL:", error);
+    throw error;
   }
 }
+
+// export async function connectToDatabase() {
+//   try {
+//     await mongoose.connect();
+//     await mongoose.connection.db.admin().command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } catch (error) {
+//     console.error("[Server] Error: " + error);
+//     process.exit(1);
+//   }
+// }
 
 // export async function disconnectFromDatabase() {
 //   try {
